e8faa0227ed7856b4b89590a64841e99cc427c83
wnkinc <140204064+wnkinc@users.noreply.github.com>
2024-08-22 23:08:49 -0700

    Wrap factory functions in an IIFE

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
-function Gameboard() {
-    const rows = 3;
-    const columns = 3;
-    const board = [];
-
-    for (let i = 0; i < rows; i++) {
-        board[i] = [];
-        for (let j = 0; j < columns; j++) {
-            board[i].push(Cell());
+const GameboardModule = (() => {
+    function Gameboard() {
+        const rows = 3;
+        const columns = 3;
+        const board = [];
+    
+        for (let i = 0; i < rows; i++) {
+            board[i] = [];
+            for (let j = 0; j < columns; j++) {
+                board[i].push(Cell());
+            }
         }
-    }
-
-    const getBoard = () => board;
-
-    const makeMove = (row, column, player) => {
-        const availableCell = board[row][column];
     
-        if (availableCell.getValue() !== 0) return;
+        const getBoard = () => board;
     
-        availableCell.addPlayerMove(player);
+        const makeMove = (row, column, player) => {
+            const availableCell = board[row][column];
+    
+            if (availableCell.getValue() !== 0) return;
+    
+            availableCell.addPlayerMove(player);
+        };
+    
+    
+        const printBoard = () => {
+            const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()));
+            console.log(boardWithCellValues);
+        };
+    
+        return { getBoard, makeMove, printBoard };
     };
-    
 
-    const printBoard = () => {
-        const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()));
-        console.log(boardWithCellValues);
+    return {
+        Gameboard
     };
-
-    return { getBoard, makeMove, printBoard };
-}
+})();
 
 function Cell() {
     let value = 0;
@@ -44,89 +50,95 @@ function Cell() {
     };
 }
 
-function GameController(
-    playerOneName = "Player One",
-    playerTwoName = "Player Two"
-) {
-    const board = Gameboard();
-
-    const players = [
-        {
-            name: playerOneName,
-            token: 1
-        },
-        {
-            name: playerTwoName,
-            token: 2 
-        }
-    ];
-
-    let activePlayer = players[0];
-
-    const switchPlayerTurn = () => {
-        activePlayer = activePlayer === players[0] ? players[1] : players[0];
-    };
-    const getActivePlayer = () => activePlayer;
-
-    const printNewRound = () => {
-        board.printBoard();
-        console.log(`${getActivePlayer().name}'s turn`);
-    };
-
-    const playRound = (row, column) => {
-        console.log(`${getActivePlayer().name}'s move into row ${row} and column ${column}.`);
-        board.makeMove(row, column, getActivePlayer().token);
-
-        const gameBoard = board.getBoard();
-
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
-                // row win check
-                if (j + 2 < 3 &&
-                    gameBoard[i][j].getValue() !== 0 && 
-                    gameBoard[i][j].getValue() === gameBoard[i][j + 1].getValue() && 
-                    gameBoard[i][j].getValue() === gameBoard[i][j + 2].getValue()) {
-                        const winningPlayer = gameBoard[i][j].getValue() === 1 ? players[0].name : players[1].name;
-                        console.log(`${winningPlayer} is the winner!`);
-                };
-                // column win check
-                if (i + 2 < 3 &&
-                    gameBoard[i][j].getValue() !== 0 && 
-                    gameBoard[i][j].getValue() === gameBoard[i + 1][j].getValue() &&
-                    gameBoard[i][j].getValue() === gameBoard[i + 2][j].getValue()) {
-                        const winningPlayer = gameBoard[i][j].getValue() === 1 ? players[0].name : players[1].name;
-                        console.log(`${winningPlayer} is the winner!`);
-                };
-                // diagonal to the right win check
-                if (j === 0 &&
-                    i === 0 &&
-                    gameBoard[i][j].getValue() === gameBoard[i + 1][j + 1].getValue() &&
-                    gameBoard[i][j].getValue() === gameBoard[i + 2][j + 2].getValue()) {
-                        const winningPlayer = gameBoard[i][j].getValue() === 1 ? players[0].name : players[1].name;
-                        console.log(`${winningPlayer} is the winner!`);
-                };
-                // diagonal to the left win check
-                if (j === 2 &&
-                    i === 0 &&
-                    gameBoard[i][j].getValue() === gameBoard[i + 1][j - 1].getValue() &&
-                    gameBoard[i][j].getValue() === gameBoard[i + 2][j - 2].getValue()) {
-                        const winningPlayer = gameBoard[i][j].getValue() === 1 ? players[0].name : players[1].name;
-                        console.log(`${winningPlayer} is the winner!`);
+const GameControllerModule = (() => {
+    function GameController(
+        playerOneName = "Player One",
+        playerTwoName = "Player Two"
+    ) {
+        const board = GameboardModule.Gameboard();
+    
+        const players = [
+            {
+                name: playerOneName,
+                token: 1
+            },
+            {
+                name: playerTwoName,
+                token: 2
+            }
+        ];
+    
+        let activePlayer = players[0];
+    
+        const switchPlayerTurn = () => {
+            activePlayer = activePlayer === players[0] ? players[1] : players[0];
+        };
+        const getActivePlayer = () => activePlayer;
+    
+        const printNewRound = () => {
+            board.printBoard();
+            console.log(`${getActivePlayer().name}'s turn`);
+        };
+    
+        const playRound = (row, column) => {
+            console.log(`${getActivePlayer().name}'s move into row ${row} and column ${column}.`);
+            board.makeMove(row, column, getActivePlayer().token);
+    
+            const gameBoard = board.getBoard();
+    
+            for (let i = 0; i < 3; i++) {
+                for (let j = 0; j < 3; j++) {
+                    // row win check
+                    if (j + 2 < 3 &&
+                        gameBoard[i][j].getValue() !== 0 &&
+                        gameBoard[i][j].getValue() === gameBoard[i][j + 1].getValue() &&
+                        gameBoard[i][j].getValue() === gameBoard[i][j + 2].getValue()) {
+                            const winningPlayer = gameBoard[i][j].getValue() === 1 ? players[0].name : players[1].name;
+                            console.log(`${winningPlayer} is the winner!`);
+                    };
+                    // column win check
+                    if (i + 2 < 3 &&
+                        gameBoard[i][j].getValue() !== 0 &&
+                        gameBoard[i][j].getValue() === gameBoard[i + 1][j].getValue() &&
+                        gameBoard[i][j].getValue() === gameBoard[i + 2][j].getValue()) {
+                            const winningPlayer = gameBoard[i][j].getValue() === 1 ? players[0].name : players[1].name;
+                            console.log(`${winningPlayer} is the winner!`);
+                    };
+                    // diagonal to the right win check
+                    if (j === 0 &&
+                        i === 0 &&
+                        gameBoard[i][j].getValue() === gameBoard[i + 1][j + 1].getValue() &&
+                        gameBoard[i][j].getValue() === gameBoard[i + 2][j + 2].getValue()) {
+                            const winningPlayer = gameBoard[i][j].getValue() === 1 ? players[0].name : players[1].name;
+                            console.log(`${winningPlayer} is the winner!`);
+                    };
+                    // diagonal to the left win check
+                    if (j === 2 &&
+                        i === 0 &&
+                        gameBoard[i][j].getValue() === gameBoard[i + 1][j - 1].getValue() &&
+                        gameBoard[i][j].getValue() === gameBoard[i + 2][j - 2].getValue()) {
+                            const winningPlayer = gameBoard[i][j].getValue() === 1 ? players[0].name : players[1].name;
+                            console.log(`${winningPlayer} is the winner!`);
+                    };
                 };
             };
+    
+    
+            switchPlayerTurn();
+            printNewRound();
         };
-
-
-        switchPlayerTurn();
+    
         printNewRound();
-    };
-
-    printNewRound();
+    
+        return {
+            playRound,
+            getActivePlayer
+        };
+    }
 
     return {
-        playRound,
-        getActivePlayer
+        GameController
     };
-}
+})();
 
-const game = GameController();
+const game = GameControllerModule.GameController();
